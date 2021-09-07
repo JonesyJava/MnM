47643385579d8472fb25750962355eccba8f083e
Michael Jones <JonesyJava@gmail.com>
2021-09-07 14:33:01 -0600

    profile and doc repo/service complete

--- Repositories/DoctorsRepository.cs
+++ Repositories/DoctorsRepository.cs
@@ -1,7 +1,83 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
+using MnM.Models;
+
 namespace MnM.Repositories
 {
     public class DoctorsRepository
     {
-        
+        private readonly IDbConnection _db;
+        public DoctorsRepository(IDbConnection db)
+        {
+            _db = db;
+        }
+
+
+        internal IEnumerable<Doctor> GetAll()
+        {
+            string sql = @"
+            SELECT 
+            doc.*,
+            prof.*
+            FROM doctors doc
+            JOIN profiles prof ON doc.creatorId = prof.id";
+            return _db.Query<Doctor, Profile, Doctor>(sql, (doctor, profile) =>
+            {
+                doctor.Creator = profile;
+                return doctor;
+            }, splitOn: "id");
+        }
+
+        internal Doctor GetById(int id)
+        {
+            string sql = @" 
+            SELECT 
+            doc.*,
+            prof.*
+            FROM doctors doc
+            JOIN profiles prof ON doc.creatorId = prof.id
+            WHERE doc.id = @id";
+            return _db.Query<Doctor, Profile, Doctor>(sql, (doctor, profile) =>
+            {
+                doctor.Creator = profile;
+                return doctor;
+            }, new { id }, splitOn: "id").FirstOrDefault();
+        }
+
+        internal Doctor Create(Doctor newDoc)
+        {
+            string sql = @"
+            INSERT INTO doctors 
+            (name, specialty, creatorId) 
+            VALUES 
+            (@Name, @Specialty, @CreatorId);
+            SELECT LAST_INSERT_ID();";
+            int id = _db.ExecuteScalar<int>(sql, newDoc);
+            newDoc.Id = id;
+            return newDoc;
+        }
+
+        internal Doctor Edit(Doctor updated)
+        {
+            string sql = @"
+            UPDATE doctors
+            SET
+            name = @Name,
+            specialty = @Specialty,
+            WHERE id = @Id;";
+            _db.Execute(sql, updated);
+            return updated;
+        }
+
+        internal void Delete(int id)
+        {
+            string sql = "DELETE FROM doctors WHERE id = @id LIMIT 1;";
+            _db.Execute(sql, new { id });
+        }
+
+
+
     }
 }
--- Services/DoctorsService.cs
+++ Services/DoctorsService.cs
@@ -1,7 +1,61 @@
+using System;
+using System.Collections.Generic;
+using MnM.Models;
+using MnM.Repositories;
+
 namespace MnM.Services
 {
     public class DoctorsService
     {
-        
+        private readonly DoctorsRepository _drepo;
+
+        public DoctorsService(DoctorsRepository drepo)
+        {
+            _drepo = drepo;
+        }
+
+        internal IEnumerable<Doctor> GetAll()
+        {
+            return _drepo.GetAll();
+        }
+
+        internal Doctor GetById(int id)
+        {
+            var data = _drepo.GetById(id);
+            if (data == null)
+            {
+                throw new Exception("Invalid Id");
+            }
+            return data;
+        }
+
+        internal Doctor Create(Doctor newDoc)
+        {
+            return _drepo.Create(newDoc);
+        }
+
+        internal Doctor Edit(Doctor updated)
+        {
+            var original = GetById(updated.Id);
+            if (original.CreatorId != updated.CreatorId)
+            {
+                throw new Exception("Invalid Edit Permissions");
+            }
+            updated.Specialty = updated.Specialty != null ? updated.Specialty : original.Specialty;
+            updated.Name = updated.Name != null && updated.Name.Length > 2 ? updated.Name : original.Name;
+            return _drepo.Edit(updated);
+        }
+
+
+        internal string Delete(int id, string userId)
+        {
+            var original = GetById(id);
+            if (original.CreatorId != userId)
+            {
+                throw new Exception("Invalid Delete Permissions");
+            }
+            _drepo.Delete(id);
+            return "delorted";
+        }
     }
 }
